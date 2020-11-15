bc46c4729761f96472c64756e508ea034543b13b
Familia Porcel <brandon7.7porcel@gmail.com>
2020-11-15 13:09:40 -0300

    ya termine esta parte,tendria que arreglar el aside(o no..)

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const d = document;
 const $nextBtn = d.getElementById('next-btn');
-const $prevBtn = d.getElementById('next-btn');
-const cuurentCharacter = 6;
+const $prevBtn = d.getElementById('prev-btn');
+let currentCharacter = 115;
 const drawChacterInfo = (dataJsonn) => {
 	const data = dataJsonn[0];
 	d.getElementById('character-image').src = data.img;
@@ -16,7 +16,6 @@ const drawChacterInfo = (dataJsonn) => {
 	d.getElementById(
 		'character-label-status'
 	).innerHTML = `Status: ${data.status}`;
-	console.log(data);
 };
 const fetchAPI = async (idCharacter) => {
 	const URL = 'https://breakingbadapi.com/api/characters';
@@ -24,6 +23,22 @@ const fetchAPI = async (idCharacter) => {
 	const answerJson = await respuesta.json();
 	drawChacterInfo(answerJson);
 };
+d.addEventListener('click', (e) => {
+	if (e.target === $prevBtn) {
+		currentCharacter -= 1;
+		if (currentCharacter < 1) {
+			currentCharacter = 117;
+		}
+		fetchAPI(currentCharacter);
+	}
+	if (e.target === $nextBtn) {
+		currentCharacter += 1;
+		if (currentCharacter > 117) {
+			currentCharacter = 1;
+		}
+		fetchAPI(currentCharacter);
+	}
+});
 d.addEventListener('DOMContentLoaded', () => {
-	fetchAPI(cuurentCharacter);
+	fetchAPI(currentCharacter);
 });
