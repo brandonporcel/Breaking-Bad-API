4743b03620e5094fd46fcde94b6bc7098408c0b0
Familia Porcel <brandon7.7porcel@gmail.com>
2020-11-15 12:38:22 -0300

    ahi estoy agregando la info,pero la forma no me convence

--- js/script.js
+++ js/script.js
@@ -6,11 +6,20 @@ const drawImage = (dataJsonn) => {
 	const data = dataJsonn[0];
 	d.getElementById('character-image').src = data.img;
 };
+const drawName = (dataJsonn) => {
+	d.getElementById('character-name').innerHTML = dataJsonn[0].name;
+};
+const drawPlaceholderName = (dataJsonn) => {
+	d.getElementById('placheholder-character-name').innerHTML = dataJsonn[0].name;
+};
+
 const fetchAPI = async (idCharacter) => {
 	const URL = 'https://breakingbadapi.com/api/characters';
 	const respuesta = await fetch(`${URL}/${idCharacter}`);
 	const answerJson = await respuesta.json();
 	drawImage(answerJson);
+	drawName(answerJson);
+	drawPlaceholderName(answerJson);
 };
 d.addEventListener('DOMContentLoaded', () => {
 	fetchAPI(cuurentCharacter);
